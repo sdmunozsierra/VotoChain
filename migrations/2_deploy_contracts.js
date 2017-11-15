efa03a9e76593539ffa0ef9ce48f9c0bf2f9bd77
Carlos Jimenez <cjimenez23@miners.utep.edu>
2017-11-15 13:33:39 -0700

    commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,9 +6,8 @@ module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
-<<<<<<< HEAD
-};
-=======
   deployer.deploy(Votar);
+// HEAD
 };
->>>>>>> 7848437d094b3971e8d6aa065e836db019184bd3
+///////////////////////////////
+// 7848437d094b3971e8d6aa065e836db019184bd3
